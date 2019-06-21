fa6703ad27836546c694145ff1e6ed376f004b0c
BreakTheCage <sarbaztanha707@outlook.com>
2019-06-21 17:49:14 +0400

    #Start of episode 148: constrolling navigation

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,4 +1,4 @@
-import { resolve } from "path";
+
 
 export class AuthService {
   loggedIn = false;
@@ -20,52 +20,3 @@ export class AuthService {
     this.loggedIn = false;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export class AuthService {
-//   loggedIn = false;
-
-//   isAuthenticated() {
-//     const promise = new Promise(
-//       (resolve, reject) => {
-//         setTimeout(() => {
-//           resolve(this.loggedIn);
-//         }, 800);
-//       }
-//     );
-//     return promise;
-//   }
-
-//   login() {
-//     this.loggedIn = true;
-//   }
-
-//   logout() {
-//     this.loggedIn = false;
-//   }
-// }
