3eb6e9db49f1b202d3a15e9884c016770faf0c82
SaeedYaghuti <sarbaztanha707@gmail.com>
2019-06-21 15:34:47 +0400

    #Start episode 146: Protecting Child

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,14 @@
+import { resolve } from "path";
+
 export class AuthService {
   loggedIn = false;
 
   isAuthenticated() {
-    const promise = new Promise(
-      (resolve, reject) => {
-        setTimeout(() => {
-          resolve(this.loggedIn);
-        }, 800);
-      }
-    );
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 800)
+    });
     return promise;
   }
 
@@ -20,3 +20,52 @@ export class AuthService {
     this.loggedIn = false;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export class AuthService {
+//   loggedIn = false;
+
+//   isAuthenticated() {
+//     const promise = new Promise(
+//       (resolve, reject) => {
+//         setTimeout(() => {
+//           resolve(this.loggedIn);
+//         }, 800);
+//       }
+//     );
+//     return promise;
+//   }
+
+//   login() {
+//     this.loggedIn = true;
+//   }
+
+//   logout() {
+//     this.loggedIn = false;
+//   }
+// }
