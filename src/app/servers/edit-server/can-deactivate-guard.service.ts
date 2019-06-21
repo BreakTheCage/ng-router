82d90207dd241e05f340efd459b6e383772fe022
BreakTheCage <sarbaztanha707@outlook.com>
2019-06-21 21:49:07 +0400

    #Start episode 149: Passing static data

--- src/app/servers/edit-server/can-deactivate-guard.service.ts
+++ src/app/servers/edit-server/can-deactivate-guard.service.ts
@@ -1,16 +1,62 @@
-import { Observable } from 'rxjs/Observable';
-import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from "rxjs";
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 
 export interface CanComponentDeactivate {
-  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean ;
 }
 
 export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
 
-  canDeactivate(component: CanComponentDeactivate,
-                currentRoute: ActivatedRouteSnapshot,
-                currentState: RouterStateSnapshot,
-                nextState?: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  canDeactivate(component: CanComponentDeactivate, 
+                currentRoute: ActivatedRouteSnapshot, 
+                currentState: RouterStateSnapshot, 
+                nextState?: RouterStateSnapshot):  Observable<boolean> | Promise<boolean> | boolean {
+    
     return component.canDeactivate();
   }
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Observable } from 'rxjs/Observable';
+// import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+// export interface CanComponentDeactivate {
+//   canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+// }
+
+// export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
+
+//   canDeactivate(component: CanComponentDeactivate,
+//                 currentRoute: ActivatedRouteSnapshot,
+//                 currentState: RouterStateSnapshot,
+//                 nextState?: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+//     return component.canDeactivate();
+//   }
+// }
--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,65 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 import { ServersService } from '../servers.service';
 import { log } from 'util';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
+  
   paramSubscription: Subscription;
   queryParamSubscription: Subscription;
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changedSaved = false;
 
   constructor(private serversService: ServersService, 
-              private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
-    console.log(this.activatedRoute.snapshot.queryParams);
-    console.log(this.activatedRoute.snapshot.fragment);
-
     this.activatedRoute.queryParams.subscribe((queryParams: Params) => {
       this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
       console.log('QueryParams: ', queryParams, 'Allow Edit: ', this.allowEdit);
     });
     this.activatedRoute.fragment.subscribe();
+    let id = +this.activatedRoute.snapshot.params['id'];
+    this.activatedRoute.params.subscribe((params: Params) => {
+      id = +params['id'];
+    });
+    this.server = this.serversService.getServer(id);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changedSaved = true;
+    console.log('@edit-server.comp: @onUpdateServer: current activedRoute: =>',this.activatedRoute)
+    this.router.navigate(['../'], {relativeTo: this.activatedRoute})
+  }
+
+  canDeactivate(): boolean | Observable<boolean> | Promise<boolean> {
+    if(!this.allowEdit){
+      return true;
+    }
+    if((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changedSaved){
+      return confirm('Do you want to discard the changes?');
+    }else {
+      return true;
+    }
   }
 
 }
 
-//=======
-//http://localhost:4200/servers/5/edit?allowEdit=1#loading
-
-//=======
-//routing by navigation method: more inside method
-// this.router.navigate(['servers', id, 'edit'], {relativeTo: this.activatedRoute, queryParams: {'allowEdit': '1'}, fragment: 'loading'})
-// //get active route parameters, query parameters, fragment
-// this.activatedRoute.snapshot.params['id']
-// this.activatedRoute.snapshot.queryParams
-// this.activatedRoute.snapshot.fragment
-// //get active route parameters with observeble subscription
-
-// paramSubscription: Subscription;
-// this.paramSubscription = this.activatedRoute.params.subscribe((params: Params ) =>{
-//   this.user.id = params.id;
-//   this.user.name = params.name;
-// })
-// //get active route Query parameters with observeble subscription
-// import { Subscription } from 'rxjs/Subscription';
-// queryParamSubscription: Subscription;
-// this.queryParamSubscription = this.activatedRoute.queryParams.subscribe((params: Params ) =>{
-//   this.user.id = params.id;
-//   this.user.name = params.name;
-// })
-// ngOnDestroy(): void {this.paramSubscription.unsubscribe();}
