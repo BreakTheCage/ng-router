3f52fb6f16b91aa13e1578c5982c7f6ee10316ac
SaeedYaghuti <sarbaztanha707@gmail.com>
2019-06-22 00:23:13 +0400

    #Start Episode 151: understanding Location

--- src/app/servers/server/server-resolver.service.ts
+++ src/app/servers/server/server-resolver.service.ts
@@ -1,20 +1,72 @@
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import { Injectable } from '@angular/core';
-
-import { ServersService } from '../servers.service';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { Observable } from "rxjs/Observable";
+import { ServersService } from "../servers.service";
+import { Injectable } from "@angular/core";
 
 interface Server {
-  id: number;
-  name: string;
-  status: string;
+  id: number,
+  name: string,
+  status: string
 }
 
+//Id we want to inject a service inside a service
 @Injectable()
 export class ServerResolver implements Resolve<Server> {
-  constructor(private serversService: ServersService) {}
-
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Server> | Promise<Server> | Server {
+  
+  constructor(private serversService: ServersService){}
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+      Server | Observable<Server> | Promise<Server> {
     return this.serversService.getServer(+route.params['id']);
   }
+  
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+// import { Observable } from 'rxjs/Observable';
+// import { Injectable } from '@angular/core';
+
+// import { ServersService } from '../servers.service';
+
+// interface Server {
+//   id: number;
+//   name: string;
+//   status: string;
+// }
+
+// @Injectable()
+// export class ServerResolver implements Resolve<Server> {
+//   constructor(private serversService: ServersService) {}
+
+//   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Server> | Promise<Server> | Server {
+//     return this.serversService.getServer(+route.params['id']);
+//   }
+// }
